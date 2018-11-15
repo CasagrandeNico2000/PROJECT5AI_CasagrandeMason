8ea5a23b06ceb60c087d0858db7711114a4ace39
Lab Mac <labmac@grr.la>
2018-11-15 09:37:03 +0100

    Mod per merge

--- ViewController.swift
+++ ViewController.swift
@@ -10,40 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var lblCompito: UILabel!
-    @IBOutlet weak var lblRisultato: UILabel!
-    
-    @IBOutlet weak var textfieldLato: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    var num = 0
-    var area = 0
-    
-    @IBAction func CalcolaAreaQ(_ sender: UIButton) {
-        self.Calcola()
-    }
-    
-    func fromStringToInt (variabile: String) -> Int {
-        var risultato = 0
-        risultato = Int(variabile)!
-        return risultato
-    }
-    
-    func calcolaAreaQuadrato(lato: Int)-> Int{
-        var area = 0
-        area = lato * lato
-        return area
-    }
-    
-    func Calcola()
-    {
-        num = fromStringToInt(variabile: textfieldLato.text!)
-        area = calcolaAreaQuadrato(lato: num)
-        lblRisultato.text = String(area)
-    }
 }
 
